08f0503aa4df709910d52ee3522c410b5da8dff3
kira <kdiradoorian0323@gmail.com>
2024-12-06 19:45:46 -0800

    Update Controller.h

--- Controller.h
+++ Controller.h
@@ -4,48 +4,57 @@
 #include <iostream>
 #include <GL/freeglut.h>
 #include "AppController.h"
-#include "Square.h"
 #include "Game.h"
-#include "GameBoard.h"
+#include "StartScreen.h"
 
-class Controller : public AppController {
+class Controller : public AppController{
     Game game;
-    Square state;
-    GameBoard gameBoard;
+    StartScreen start;
+
+    bool gameStarted;
 
 public:
-    Controller(){
-        
-    }
+    Controller() : gameStarted(false) {}
 
     void render() {
-        Game();
+        if(!gameStarted) {
+            start.draw();
+        } else {
+            game.gameScreen();
+        }
     }
 
-    // Children can optionally define functions listed below
-    void leftMouseDown(float x, float y) {
-        gameBoard.handleMouseClick(x, y);
-    };
+    void leftMouseDown(int button, int state, float x, float y) {
+        if(!gameStarted) {
+            start.handleMouseClick(button, state, x, y);
+            if(start.startGame()) {
+                if(start.threeSelected()) {
+                    game.resizeBoard(3);
+                } else if(start.fourSelected()) {
+                    game.resizeBoard(4);
+                } else if(start.fiveSelected()) {
+                    game.resizeBoard(5);
+                }
+
+                if(start.isEasySelected()) {
+                    game.setEasyAI(true);
+                } else if(start.isMediumSelected()) {
+                    game.setMediumAI(true);
+                } else if(start.isHardSelected()) {
+                    game.setHardAI(true);
+                }
+                gameStarted = true;
+                game.resetGame();
+            }
+        } else {
+            game.handleMouseClick(button, state, x, y);
+        }
+    }
     
-    void leftMouseUp(float x, float y) {};
-
-    void rightMouseDown(float x, float y) {};
-    void rightMouseUp(float x, float y) {};
-
-    void mouseMotion(float x, float y) {};
-    
-    void keyboardDown(unsigned char key, float x, float y) {};
-    
-
-    // void leftMouseDown(float mx, float my) {
-    //     game.handleMouseClick(mx, my);
-    // }
-
-    // void sharePlayer(){
-    //     game.checkWin(state.getState())
-    // }
+    void leftMouseUp(float x, float y) {}
 
     ~Controller(){}
+    
 };
 
 #endif
